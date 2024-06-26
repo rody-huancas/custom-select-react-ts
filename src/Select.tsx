98717abe1bf17f7102ffdf443dbb04a810782de6
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-25 19:56:18 -0500

    Add: single select html

---
+++ src/Select.tsx
@@ -0,0 +1,30 @@
+import styles from "./select.module.css";
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+type SelectProps = {
+  options: SelectOption[];
+  value?: SelectOption;
+  onChange: (value: SelectOption | undefined) => void;
+};
+
+export const Select = ({ value, onChange, options }: SelectProps) => {
+  return (
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}>value</span>
+      <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={styles.options}>
+        {options.map((option) => (
+          <li key={option.value} className={styles.option}>
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
